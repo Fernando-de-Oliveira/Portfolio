c7d9eb06de63e9c0b0eff14c7e583afe2636138d
Fernando Laurino <fernandolaurino28@gmail.com>
2025-02-13 16:43:44 -0300

    Melhorias skills

--- src/app/shared/skillset-menu/skillset-menu.component.ts
+++ src/app/shared/skillset-menu/skillset-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RadialMenuComponent } from '../radial-menu/radial-menu.component';
 import { Skill, SKILLS_DATA } from '../../consts/skillsConsts';
@@ -9,11 +9,13 @@ import { Skill, SKILLS_DATA } from '../../consts/skillsConsts';
   styleUrls: ['./skillset-menu.component.scss'],
   imports:[CommonModule, RadialMenuComponent]
 })
-export class SkillsetMenuComponent {
+export class SkillsetMenuComponent{
+@ViewChild('container', { static: false}) container!: ElementRef;
+@ViewChild('lastRow', { static: false}) lastRow!: ElementRef;
   
 skills: Skill[] = SKILLS_DATA
 selectedSkill: any = null;
-
+scrolled = false
 getSkillPosition(index: number): string {
   const total = this.skills.length;
   const angle = (360 / total) * index;
@@ -31,6 +33,7 @@ getSkillPosition(index: number): string {
   }
   return `rotate(${angle}deg) translate(${distance}px) rotate(-${angle}deg)`;
 }
+angleStep = 360 / 8;
 
   getIconPosition(index: number) {
     const angle = (360 / this.skills.length) * index;
@@ -42,7 +45,35 @@ getSkillPosition(index: number): string {
   }
 
   toggleRadialMenu(skill: any) {
-    this.selectedSkill = this.selectedSkill == skill ? null : skill;
-    
+
+    let rotationAngle = 0;
+    const targetAngle = 180 - (this.angleStep * skill.index);
+    rotationAngle = targetAngle;
+    if (typeof window !== 'undefined'){
+      const circles = this.container.nativeElement.querySelectorAll('.circle'); 
+      const screenWidth = window.innerWidth;    
+      if (screenWidth < 1280) {
+        rotationAngle-= 90;
+        if (!this.scrolled) {
+          setTimeout(() => {
+            this.lastRow.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
+          }, 100);
+          this.scrolled = true
+        }
+      } 
+      circles.forEach((circle: HTMLElement, index: number) => {
+        const angle = (360 / this.skills.length) * index;
+        if (screenWidth < 1280) {
+          circle.style.transform = `rotate(${angle - 90}deg)`;
+        } else {
+          let angleCircle = 180 - (this.angleStep * skill.index);
+          circle.style.transform = `rotate(${-angleCircle}deg)`;
+        }
+      });
+    }
+  this.container.nativeElement.style.transform = `rotate(${rotationAngle}deg)`;
+  this.selectedSkill = this.selectedSkill == skill ? null : skill;
+ 
+
   }
 }
